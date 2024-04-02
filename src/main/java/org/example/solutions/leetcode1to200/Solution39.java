467d5673682e273132312de1c11ec1af5a9019b5
FantasyShaw <609465835@qq.com>
2024-04-02 17:01:55 +0800

    Update 4.2.2024

---
+++ src/main/java/org/example/solutions/leetcode1to200/Solution39.java
@@ -0,0 +1,29 @@
+package org.example.solutions.leetcode1to200;
+
+import java.util.*;
+
+public class Solution39 {
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> cur = new ArrayList<>();
+            dfs(candidates, target, 0, ans, cur);
+            return ans;
+        }
+
+        private void dfs(int[] nums, int target, int left, List<List<Integer>> ans, List<Integer> cur) {
+            if (target < 0) {
+                return;
+            }
+            if (target == 0) {
+                ans.add(new ArrayList<>(cur));
+                return;
+            }
+            for (int i = left; i < nums.length; i++) {
+                cur.add(nums[i]);
+                dfs(nums, target - nums[i], i, ans, cur);
+                cur.removeLast();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/solutions/leetcode501to1k/Solution894.java
@@ -0,0 +1,32 @@
+package org.example.solutions.leetcode501to1k;
+
+import org.example.utils.TreeNode;
+
+import java.util.*;
+
+public class Solution894 {
+    class Solution {
+        public List<TreeNode> allPossibleFBT(int n) {
+            List<TreeNode>[] memo = new List[n + 1];
+            memo[1] = List.of(new TreeNode());
+            return dfs(n, memo);
+        }
+
+        private List<TreeNode> dfs(int n, List<TreeNode>[] memo) {
+            if (memo[n] != null) {
+                return memo[n];
+            }
+            List<TreeNode> ans = new ArrayList<>();
+            for (int i = 0; i < n - 1; i++) {
+                int j = n - i - 1;
+                for (var l : dfs(i, memo)) {
+                    for (var r : dfs(j, memo)) {
+                        ans.add(new TreeNode(0, l, r));
+                    }
+                }
+            }
+            memo[n] = ans;
+            return ans;
+        }
+    }
+}
